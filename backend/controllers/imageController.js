3bfbe38f2f76437c87285a7b04ecab79e5b551f7
oonyoontong <oontong_tan@mymail.sutd.edu.sg>
2018-06-23 14:17:02 +0800

    adding backend

---
+++ backend/controllers/imageController.js
@@ -0,0 +1,73 @@
+const connection = {
+    host: "devostrum.no-ip.info",
+    user: "oracle",
+    password: "oracle",
+    database: "oracle"
+};
+
+const mysql = require('mysql');
+const con = mysql.createConnection(connection)
+
+con.connect();
+
+var spawn = require("child_process").spawn;
+
+var options = {
+    cwd: process.cwd(),
+    env: process.env,
+};
+
+
+
+/*Call Isaac's function then send to DB*/
+exports.new_image = function(req,res,next){
+    // res.send()
+    imageBase64 = req.body["imageBase64"];
+    width = req.body["width"];
+    height = req.body["height"];
+    cameraId = req.body["cameraId"];
+    // start child yolo process
+/*    var child = spawn('python3', ['process_image.py', imageBase64, width, height,cameraId], options);
+
+    // save all the outputs
+    child.stdout.on('data', function(data) {
+        console.log(data.toString());
+    });
+
+    // log error if any
+    child.stderr.on('data', function(data) {
+        console.log("ERR child process: " + data.toString());
+        yoloErr = "Generator Algorithm: " + data.toString();
+    });
+
+    // once child process ends, update SQL table
+    child.on('close', function(code) {
+        if (!yoloErr) {
+
+        } else {
+            res.json({"success":false, "message":yoloErr});
+        }
+    });*/
+};
+
+/*Query DB*/
+exports.query = function(req,res,next){
+    res.send("QUERY DB")
+};
+
+/*exports.read_result_from_id = function (req, res, next) {
+    // Return all Result
+    if (typeof(req.query) === "undefined" || typeof (req.query.id) === "undefined") {
+        var query = "SELECT * FROM Result WHERE isDeleted = FALSE";
+    }
+    //Return only the Result with the given routineId
+    else {
+        var query = "SELECT * FROM Result WHERE `id` = \"" +
+            req.query.id + "\" AND isDeleted = FALSE";
+    }
+    con.query(query, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+}*/
