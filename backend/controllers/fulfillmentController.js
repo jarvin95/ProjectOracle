365d19b3f78e70fdde57333e818eb6462f8e1ea9
Jarvin <jarvinong@gmail.com>
2018-06-23 22:14:59 +0800

    Fixed SQL error

--- backend/controllers/fulfillmentController.js
+++ backend/controllers/fulfillmentController.js
@@ -40,7 +40,7 @@ function retrieve(item_string) {
     var query1 = "SELECT `id` FROM Object WHERE name = \"" + item_string + "\";";
     con.query(query1, function (err, result1) {
         if (err) throw err;
-        var query2 = "SELECT TOP 1 `reference_object` FROM Instance WHERE `parent_object_id` = \"" + result1 + "\";";
+        var query2 = "SELECT `reference_object` FROM Instance WHERE `parent_object_id` = \"" + result1 + "\" ORDER BY ID DESC LIMIT 1;";
         con.query(query2, function(err, result2) {
             if (err) throw err;
             return result2;
