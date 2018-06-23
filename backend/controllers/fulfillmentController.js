eb732b04421f6072925153b5c624bc2e0fa77823
Jarvin <jarvinong@gmail.com>
2018-06-23 20:58:59 +0800

    Test response from service

--- backend/controllers/fulfillmentController.js
+++ backend/controllers/fulfillmentController.js
@@ -19,15 +19,67 @@ exports.fulfill = function (req, res, next) {
     console.log(param + " " + intent_name);
 
     var query = "SELECT * FROM Object WHERE name = \"" + item_name + "\"";
+    /*var query = "SELECT * FROM Object WHERE name = \'" + item_name;
     con.query(query, function(err, result) {
         if (err) throw err;
         console.log(result);
         res.json(result);
-    })
+    })*/
 
-
-    /*res.json({
-        'param': param,
-        'intent_name': intent_name
-    });*/
+    var response = {
+        "fulfillmentText": "This is a text response",
+        "fulfillmentMessages": [
+            {
+                "card": {
+                    "title": "card title",
+                    "subtitle": "card text",
+                    "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                    "buttons": [
+                        {
+                            "text": "button text",
+                            "postback": "https://assistant.google.com/"
+                        }
+                    ]
+                }
+            }
+        ],
+        "source": "example.com",
+        "payload": {
+            "google": {
+                "expectUserResponse": true,
+                "richResponse": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": "this is a simple response"
+                            }
+                        }
+                    ]
+                }
+            },
+            "facebook": {
+                "text": "Hello, Facebook!"
+            },
+            "slack": {
+                "text": "This is a text response for Slack."
+            }
+        },
+        "outputContexts": [
+            {
+                "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+                "lifespanCount": 5,
+                "parameters": {
+                    "param": "param value"
+                }
+            }
+        ],
+        "followupEventInput": {
+            "name": "event name",
+            "languageCode": "en-US",
+            "parameters": {
+                "param": "param value"
+            }
+        }
+    };
+    res.send(response);
 };
