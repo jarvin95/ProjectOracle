478e905bad6942ca7c6fa725c512003601d39f6a
Jarvin <jarvinong@gmail.com>
2018-06-24 13:07:10 +0800

    asoidh

--- backend/controllers/fulfillmentController.js
+++ backend/controllers/fulfillmentController.js
@@ -81,37 +81,76 @@ function responseObj(array_of_positions, param) {
         console.log(response);
         return {
             "conversationToken": "",
-            "expectUserResponse": false,
+            "expectUserResponse": true,
             "expectedInputs": [
                 {
                     "inputPrompt": {
-                        "richInitialPrompt": {
-                            "items": [
-                                {
-                                    "simpleResponse": {
-                                        "textToSpeech": "Alright! " + response
-                                    }
-                                },
-                                {
-                                    "carouselBrowse": {
-                                        "items": [
-                                            {
-                                                "title": array_of_positions[0],
-                                                "image": {
-                                                    "url": "https://example.google.com/42.png",
-                                                    "accessibilityText": "Image alternate text"
-                                                },
-                                                "imageDisplayOptions": "CROPPED"
-                                            }
-                                        ]
-                                    }
-                                }
-                            ]
-                        }
+                        "initialPrompts": [
+                            {
+                                "textToSpeech": "Alright! " + response
+                            }
+                        ],
+                        "noInputPrompts": []
                     },
                     "possibleIntents": [
                         {
-                            "intent": "assistant.intent.action.TEXT"
+                            "intent": "actions.intent.OPTION",
+                            "inputValueData": {
+                                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                                "carouselSelect": {
+                                    "items": [
+                                        {
+                                            "optionInfo": {
+                                                "key": "MATH_AND_PRIME",
+                                                "synonyms": [
+                                                    "math",
+                                                    "math and prime",
+                                                    "prime numbers",
+                                                    "prime"
+                                                ]
+                                            },
+                                            "title": "Math & prime numbers",
+                                            "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
+                                            "image": {
+                                                "url": "http://example.com/math_and_prime.jpg",
+                                                "accessibilityText": "Math & prime numbers"
+                                            }
+                                        },
+                                        {
+                                            "optionInfo": {
+                                                "key": "EGYPT",
+                                                "synonyms": [
+                                                    "religion",
+                                                    "egpyt",
+                                                    "ancient egyptian"
+                                                ]
+                                            },
+                                            "title": "Ancient Egyptian religion",
+                                            "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
+                                            "image": {
+                                                "url": "http://example.com/egypt",
+                                                "accessibilityText": "Egypt"
+                                            }
+                                        },
+                                        {
+                                            "optionInfo": {
+                                                "key": "RECIPES",
+                                                "synonyms": [
+                                                    "recipes",
+                                                    "recipe",
+                                                    "42 recipes"
+                                                ]
+                                            },
+                                            "title": "42 recipes with 42 ingredients",
+                                            "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
+                                            "image": {
+                                                "url": "http://example.com/recipe",
+                                                "accessibilityText": "Recipe"
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
                         }
                     ]
                 }
