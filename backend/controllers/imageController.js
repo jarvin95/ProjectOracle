297f44234c7551fcfa8ca5ce2123bf367a3ea7ad
Rayson Lim Jun Kai <rayson@jublia.com>
2018-06-24 09:50:44 +0800

    Asdf

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -159,12 +159,8 @@ exports.obj_crops = function(req, res, next) {
     res.writeHead(200, {'Content-Type': 'image/jpg'});
     var img = gm('./uploads/cam-2')
                 .crop(0,0, 50,50)
-                .stream(function streamOut (err, stdout, stderr) {
-                    if (err) {
-                        console.log(err);
-                        return next(err);
-                    }
-                    stdout.pipe(res);
+                .write(res, function(err) {
+                    console.log(err);
                 });
 }
 
