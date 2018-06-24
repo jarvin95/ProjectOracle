a01aa313a16afa348104248da76273633f999c1e
Rayson Lim Jun Kai <rayson@jublia.com>
2018-06-24 14:19:16 +0800

    af

--- dialogflow/app.js
+++ dialogflow/app.js
@@ -117,5 +117,5 @@ function onListening() {
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  //debug('Listening on ' + bind);
 }
