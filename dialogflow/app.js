4be604e182ca8fa17ab79cf4065656af7202c06c
Rayson Lim Jun Kai <rayson@jublia.com>
2018-06-24 14:17:56 +0800

    af

--- dialogflow/app.js
+++ dialogflow/app.js
@@ -8,9 +8,19 @@ const {
 const express = require('express')
 const bodyParser = require('body-parser')
 
+var https = require('https');
+var fs = require('fs');
+const options = {
+    cert: fs.readFileSync("/etc/letsencrypt/live/www.kodyac.tech-0001/fullchain.pem"),
+    key: fs.readFileSync("/etc/letsencrypt/live/www.kodyac.tech-0001/privkey.pem")
+};
+
+
 // Create an app instance
 const app = dialogflow()
-express().use(bodyParser.json(), app).listen(8080)
+const e = express().use(bodyParser.json(), app)
+
+
 
 // Register handlers for Dialogflow intents
 app.intent('Default Welcome Intent', conv => {
@@ -30,3 +40,82 @@ app.intent('Goodbye', conv => {
 app.intent('Default Fallback Intent', conv => {
   conv.ask(`I didn't understand. Can you tell me something else?`)
 })
+
+
+var port = normalizePort('8080');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+//var server = http.createServer(app);
+var server = https.createServer(options, e)
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
